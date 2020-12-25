dda1e24ff77b40236b1712921d34d30e7253ba0a
iambhushansomani <somani.bhushan01@outlook.com>
2020-12-25 16:38:12 +0000

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,57 @@
+﻿using DockerComposeLab.Data;
+using DockerComposeLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerComposeLab.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private DataContextClass db;
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, DataContextClass context)
+        {
+            _logger = logger;
+            db = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+        [HttpGet("findall")]
+        public IEnumerable<FavoriteBand> GetList()
+        {
+            try
+            {
+                var products = db.FavoriteBands.ToList();
+                return (products);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Data/DataContextClass.cs
@@ -0,0 +1,19 @@
+﻿using DockerComposeLab.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace DockerComposeLab.Data
+{
+    public class DataContextClass : DbContext
+    {
+        public DataContextClass(DbContextOptions<DataContextClass> options) : base(options)
+        {
+        }
+
+        public DbSet<FavoriteBand> FavoriteBands { get; set; }
+    }
+}
